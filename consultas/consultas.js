7482a123ed7451cb14a8722c171fe1993ccf5320
Betsy Toyo <btoyo6009@e-camp.cl>
2022-04-20 19:44:36 -0400

    fin del Crud

--- consultas/consultas.js
+++ consultas/consultas.js
@@ -7,19 +7,108 @@ const pool= new Pool({
     port: 5432
 })
 
-const nuevoUsuario=async(datos)=>{
-    const datosUsuarios= Object.values(datos)
+const nuevoUsuario= async(datos)=>{
+    const {nombre, balance}= datos
     const parametros={
         text:'INSERT INTO usuarios(nombre, balance) VALUES($1, $2) RETURNING*',     
-        datosUsuarios        
+        values: [nombre, balance]       
     }
 
     try {
-        const {rows}=await pool.query(parametros)
+        const { rows }=await pool.query(parametros)
         return rows
     } catch (error) {
         console.log(error.message)
     }
 }
 
-module.exports={nuevoUsuario}
+const listaUsuarios= async()=>{
+    const consultaSql= 'SELECT * FROM usuarios'
+    try {
+        const { rows }=await pool.query(consultaSql)
+        return rows
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+const editarUsuario= async(datos)=>{
+    const {name, balance, id}= datos
+    const parametros= {
+        text:'UPDATE usuarios SET nombre= $1, balance= $2 WHERE id= $3 RETURNING*',
+        values:[name, balance, id]
+    }
+    try {
+        const { rows }= await pool.query(parametros)
+        return rows
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+const eliminarUsuario= async (id)=>{
+    const sql= `DELETE FROM usuarios WHERE id=${id}`
+    try {
+        const { rows }= await pool.query(sql)
+        return rows
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+const transferencia= async(datos)=>{
+    const {emisor, receptor, monto, fecha}= datos    
+   
+    try {
+        await pool.query('BEGIN');
+
+        const idEmisor=await pool.query('SELECT id FROM usuarios WHERE nombre= $1',[emisor])
+        const idReceptor=await pool.query('SELECT id FROM usuarios WHERE nombre= $1',[receptor])
+        const paramTransfer={
+            text: 'INSERT INTO transferencias (emisor, receptor, monto, fecha) VALUES($1, $2, $3, $4) RETURNING*',
+            values:[idEmisor.rows[0].id, idReceptor.rows[0].id, monto, fecha],            
+        }
+        console.log(idEmisor.rows[0].id,idReceptor.rows[0].id)
+        const paramUsuEnvia={
+            text:'UPDATE usuarios SET balance = balance - $1 WHERE id=$2 RETURNING*',
+            values: [monto, idEmisor.rows[0].id]
+        }
+        const paramUsuRecibe={
+            text:'UPDATE usuarios SET balance= balance + $1 WHERE id=$2 RETURNING*',
+            values: [monto, idReceptor.rows[0].id]
+        }
+
+        const transferencia= await pool.query(paramTransfer)
+        const usuEnvia= await pool.query(paramUsuEnvia)
+        const usuRecibe= await pool.query(paramUsuRecibe)
+
+        await pool.query("COMMIT");
+        return{status: 'OK', transferencia: transferencia.rows[0], envia: usuEnvia.rows[0], recibe: usuRecibe.rows[0]}
+        
+    } catch (error) {
+        await pool.query("ROLLBACK");
+        console.log(error.message)
+        return { status: 'ERROR', mensaje: error.message }
+    }
+}
+
+const listarTransf= async()=>{
+    const parametros= {
+        text:`SELECT t.fecha, u.nombre, u2.nombre, monto
+                FROM transferencias t 
+                inner join usuarios u  on u.id = t.emisor
+                inner join usuarios u2  on u2.id= t.receptor `,
+        values:[],
+        rowMode: 'array'
+    }
+
+    try {
+        const { rows }= await pool.query(parametros)
+        console.log(rows)
+        return rows
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+module.exports={nuevoUsuario, listaUsuarios, editarUsuario, eliminarUsuario, transferencia, listarTransf}
--- index.js
+++ index.js
@@ -1,10 +1,85 @@
 const express= require('express');
-const { send } = require('process');
-const {nuevoUsuario}= require('./consultas/consultas')
-const app= express()
-app.listen(3000,()=>{console.log('Servidor on')})
+const moment= require('moment')
+const {nuevoUsuario, listaUsuarios, editarUsuario, eliminarUsuario, transferencia, listarTransf}= require('./consultas/consultas');
+const app= express();
+app.use(express.json());
+app.listen(3000,()=>{console.log('Servidor on')});
 
 app.get('/', (request, response)=>{
     response.sendFile(`${__dirname}/index.html`)    
+});
+
+app.post('/usuario',async(request, response)=>{
+    const {nombre, balance}= request.body
+    const parametros={
+        nombre, balance
+    }
+    try {
+       const respuesta= nuevoUsuario(parametros)
+       response.json(respuesta).status(201)
+    } catch (error) {
+        console.log(error.message)
+        response.status(500)
+    }
 })
 
+app.get('/usuarios', async (request, response)=>{
+    try {
+       const respuesta= await listaUsuarios()        
+       response.json(respuesta).status(200)
+    } catch (error) {
+        console.log(error.message)
+        response.status(500)
+    }
+})
+
+app.put('/usuario', async (request, response)=>{
+    const {name, balance}= request.body
+    const id= request.query.id
+    const parametros={
+        name, balance, id
+    }
+    try {
+        const respuesta= editarUsuario(parametros)
+        response.json(respuesta).status(201)
+    } catch (error) {
+        console.log(error.message)
+        response.status(500)
+    }
+})
+
+app.delete('/usuario', async(request, response)=>{
+    const id= request.query.id
+    try {
+        const respuesta= await eliminarUsuario(id)
+        response.json(respuesta).status(200)
+    } catch (error) {
+        console.log(error.message)
+        response.status(500)
+    }
+})
+
+app.post('/transferencia',async (request, response)=>{
+    const {emisor, receptor, monto}=request.body
+    const fecha=  moment()
+    const parametros={
+        emisor, receptor, monto, fecha
+    }
+    try {
+        const respuesta= transferencia(parametros)
+        response.json(respuesta).status(201)
+    } catch (error) {
+        console.log(error.message)
+        response.status(500)
+    }
+})
+
+app.get('/transferencias', async (request, response)=>{
+    try {
+        const respuesta= await listarTransf()
+        response.json(respuesta).status(200)
+    } catch (error) {
+        console.log(error.message)
+        response.status(500)
+    }
+})
